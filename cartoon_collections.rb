45ae7547fa6bb81115ddc6a0e2fdda95591ef6e2
Ana Riat <t.riat24@gmail.com>
2019-05-12 11:18:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves# code an argument here
   dwarves.each_with_name {|name,i|
   i+=1 
-  puts"#{i}. #{name} 
+  puts"#{i}. #{name}"
 end
 
 def summon_captain_planet# code an argument here
